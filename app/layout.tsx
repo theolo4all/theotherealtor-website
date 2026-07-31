8ea7ccb88c7fa5aaebd836f75b95dacdf41eece5
Theo Tajou <theolo4all@gmail.com>
2026-07-31 00:05:32 -0400

    Add Google Analytics integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -109,9 +110,12 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en-CA">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+return (
+  <html lang="en-CA">
+    <body className={inter.className}>
+      <GoogleAnalytics />
+      {children}
+    </body>
+  </html>
+);
 }
